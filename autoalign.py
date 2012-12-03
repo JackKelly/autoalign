251960535a2cc996958d6fabc4281096d9a4ae66
Jack Kelly <jack-list@xlk.org.uk>
2012-12-03 22:05:38 +0000

    Using simpler data structures and OOP:
    * Using numpy instead of pandas
    * Started writing classes for MeterSignal and WattsUp

--- autoalign.py
+++ autoalign.py
@@ -1,53 +1,51 @@
 from __future__ import print_function, division
-import pandas
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load timestamped data (haystack)
-# Load WattsUp data (target)
-# for i in range(0, haystack.end - target.duration):
-#    for j in range(i, :
 
-# Keep a best score and the location for that best score
+class MeterSignal(object):
+    def __init__(self, filename):
+        """Loads REDD-formatted data. Returns pandas Timeseries."""
+        print("Loading", filename)
+        self.data = np.loadtxt(open(filename, 'rU'), dtype=[('timestamp', np.uint32), ('watts', float)])
 
-def load_haystack(filename):
-    """Loads REDD-formatted data. Returns pandas Timeseries."""
-    print("Loading", filename)
-    
-    dateparser = lambda timestamp: datetime.datetime.fromtimestamp(int(timestamp))
-    return pandas.read_csv(filename, sep=" ", index_col=0, parse_dates=True, date_parser=dateparser, squeeze=True, header=None)
+    def plot(self, axes):
+        x = [datetime.datetime.fromtimestamp(t) for t in self.data['timestamp']]
+        axes.plot(x, self.data['watts'])
 
+    def find(self, watts_up):
+        best = {'score':0, 'start_index':0}
+        
+        # last_timestamp_to_search = timestamp corresponding to self.data['timestamp'][-1]-watts_up.duration
+        
+        for i in range(0, last_timestamp_to_search):
+            pass # TODO
+        
+        return best
+        
+class WattsUp(object):
+    def __init__(self, filename, period=1):
+        """Load WattsUp data"""
+        print("Loading", filename)
+        self.period = period # seconds
+        self.data = np.loadtxt(open(filename, 'rU'), delimiter="\t", dtype=float, skiprows=3, usecols=[1])
 
-def load_target(filename):
-    """Load WattsUp data"""
-    wu = pandas.read_csv(filename, sep="\t", skiprows=2, 
-                         names=["Time", "Watts", "Volts", "Amps", "WattHrs", "Cost", 
-                                "Avg Kwh", "Mo Cost", "Max Wts", "Max Vlt", "Max Amp",
-                                "Min Wts", "Min Vlt", "Min Amp", "Pwr Fct", "Dty Cyc",
-                                "Pwr Cyc", "A", "B"])
-    
-    wu = wu['Watts']
-    
-    index = pandas.Index([datetime.timedelta(seconds=int(secs)) for secs in wu.index])
-    
-    return pandas.Series(wu.values, index=index)
+    def plot(self, axes, start_timestamp=0):
+        x = [datetime.datetime.fromtimestamp(t+start_timestamp) for t in range(0, self.data.size, self.period)]
+        axes.plot(x, self.data)
 
-
-def plot_target(target, start_date, axes):
-    # x = np.array([start_date + datetime.timedelta(seconds=secs) for secs in target.index])
-    axes.plot(target.index + start_date, target.values)
-
+    @property
+    def duration(self):
+        return self.data.size * self.period
 
 if __name__ == "__main__":
-    s = load_haystack("/homes/dk3810/Dropbox/Data/data0/channel_01.dat")
-    wu = load_target("/homes/dk3810/Dropbox/Data/BellendenWMNov2012.TXT")
-    
+    haystack = MeterSignal("/home/jack/Dropbox/Data/data0/channel_01.dat")
+    target = WattsUp("/home/jack/Dropbox/Data/BellendenWMNov2012.TXT")
+       
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot(s.index, s.values)
-    
-    plot_target(wu, datetime.datetime(year=2012, month=11, day=10), ax)
-
+    haystack.plot(ax)
+    target.plot(ax, haystack.data['timestamp'][1000])
     plt.show()
     
