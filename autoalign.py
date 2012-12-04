ee5ed490925444a1d613cd2b85b47f482e44f04d
Jack Kelly <jack-list@xlk.org.uk>
2012-12-04 18:05:38 +0000

    This algo is rather too fragile to be of much use. But I used this code to produce graphs in my latest blog post: http://jack-kelly.com/how_accurate_is_an_edf_individual_appliance_monitor

--- autoalign.py
+++ autoalign.py
@@ -6,11 +6,21 @@ import matplotlib
 import numpy as np
 import sys
 
+"""
+Attempts to automatically align a WattsUp power signature to a Current Cost
+power time series.
 
-def calc_steady_states(data):
+This script is just a bit of tinkering.  The algorithm is far too basic
+and fragile to be of much use to anyone.  Find steady states, then
+steady state transitions, and then try to match these.  It would probably 
+be easier and more reliable to just look at large-magnitude forward diffs.
+"""
+
+def calc_steady_states(data, timestamps=None):
     """Return a list of 3-element tuples: (start index, end index, value)"""       
     TOL = 10 # tolerance in watts
     MIN_LEN = 12 # min length of a steady state
+    
     av = data[0] # average (mean)
     acc = data[0] # accumulator (for average)
     start_i = 0
@@ -19,14 +29,58 @@ def calc_steady_states(data):
         if av-TOL < data[i] < av+TOL:
             acc += data[i]
             av = acc / (1 + i - start_i)
-        else:
-            if (i-start_i) > MIN_LEN:
+        else:            
+            duration = i-start_i if timestamps is None else \
+                       timestamps[i]-timestamps[start_i]
+
+            if duration > MIN_LEN:
                 states.append((start_i, i, av))
             acc = data[i]
             av = data[i]
             start_i = i
 
     return states
+
+
+def calc_good_steady_state_transitions(steady_states, timestamps=None):
+    """Good state transitions are those which are large in magnitude
+    and where the two states are very close.
+    
+    Returns list of tuples for transition: 
+      - start index
+      - length (seconds)
+      - value (watts)
+      - score
+    """
+      
+    MIN_SCORE = 10
+    
+    ss_transitions = []
+    last_ss_end = 0
+    last_ss_value = 0
+    for ss_start, ss_end, ss_value in steady_states:
+
+        t_length = ss_start - last_ss_end if timestamps is None else \
+                   timestamps[ss_start] - timestamps[last_ss_end]
+                    
+        if t_length == 0:
+            t_length = 1
+            
+        t_value = ss_value - last_ss_value
+        t_score = abs(t_value) if t_length < 18 else abs(t_value)/t_length
+        last_ss_end = ss_end
+        last_ss_value = ss_value
+        
+        if t_score > MIN_SCORE:
+            ss_transitions.append((last_ss_end, t_length, t_value, t_score))
+                        
+    return ss_transitions
+
+
+def plot_steady_states(axes, steady_states):
+    for start, end, value in steady_states:
+        line = matplotlib.lines.Line2D([start, end], [value, value], color='r')
+        axes.add_line(line)
 
 
 class MeterSignal(object):
@@ -36,50 +90,75 @@ class MeterSignal(object):
         self.data = np.loadtxt(open(filename, 'rU'), dtype=[('timestamp', np.uint32), ('watts', float)])
         self.f_diff = self.data['watts'][1:] - self.data['watts'][:-1]
         self.delta_t = self.data['timestamp'][1:] - self.data['timestamp'][:-1]
-        self.steady_states = calc_steady_states(self.data['watts'])
+        self.steady_states = calc_steady_states(self.data['watts'], self.data['timestamp'])
+        self.steady_state_transitions = calc_good_steady_state_transitions(self.steady_states, self.data['timestamp'])
         print("done.")
 
     def plot(self, axes):
         x = [datetime.datetime.fromtimestamp(t) for t in self.data['timestamp']]
-        axes.plot(x, self.data['watts'])
+        axes.plot(x, self.data['watts']/1000, label="EDF IAM")
 
     def locate_signal(self, watts_up):
         if watts_up.duration >= self.duration:
             raise ValueError("WattsUp duration {} >= MeterSignal duration {}"
                              .format(watts_up.duration, self.duration))
         
-        best = {'mse': np.finfo(float).max, # set Mean Sq Error to max float val
+        best = {'mean_error': np.finfo(float).max, # set Mean Sq Error to max float val
                 'start_index': 0}
         
-        last_i = self.index_from_timestamp(self.data['timestamp'][-1] - watts_up.duration)
+        end_time_of_search_window = self.data['timestamp'][-1] - watts_up.duration
         
-        for start_i in range(0, last_i):
-            i = start_i
-            end_time = self.data['timestamp'][start_i] + watts_up.duration
-            end_i = self.index_from_timestamp(end_time)
-                       
-            se_acc = 0 # squared error accumulator
-            for t_start, t_length, t_value, t_score in watts_up.steady_state_transitions:
-                start_timestamp = self.data['timestamp'][start_i]+t_start
-                end_timestamp = start_timestamp + t_length
+        for start_i in range(0, len(self.steady_state_transitions)):            
+            start_timestamp = self.data['timestamp'][self.steady_state_transitions[start_i][0]]
+             
+            if start_timestamp > end_time_of_search_window:
+                break
+            
+            error_acc = 0 # error accumulator
+            inner_i = start_i
+            for target_transition in watts_up.steady_state_transitions:
+                # Does a transition exist in haystack t_start_i seconds away from start_timestamp, length t_length, value t_value
+                try:
+                    closest_match = self.find_transition_matching(target_transition, inner_i, start_timestamp)
+                except IndexError:
+                    error_acc += 100 # penalise misses
+                    continue
                 
-                haystack_start_i = self.index_from_timestamp(start_timestamp)
-                haystack_end_i = self.index_from_timestamp(end_timestamp)
+                error_acc += closest_match['error']
+                inner_i = closest_match['i']
                 
-                haystack_fdiff = self.data['watts'][haystack_end_i] - self.data['watts'][haystack_start_i]
-                
-                se_acc += (haystack_fdiff - t_value)**2
-                
-            mse = se_acc / len(watts_up.steady_state_transitions) # mean squared error
-            if mse < best['mse']:
-                best['mse'] = mse
+            mean_error = error_acc / len(watts_up.steady_state_transitions)
+            if mean_error and mean_error < best['mean_error']:
+                best['mean_error'] = mean_error
                 best['start_index'] = start_i
                 print(best)
                 
-            print("{:.2%}".format(start_i / self.data.size), end="\r")
+            print("{:.2%}".format(start_i / len(self.steady_state_transitions)), end="\r")
             sys.stdout.flush()
         
         return best
+    
+    def find_transition_matching(self, target_transition, i, start_timestamp):
+        best = {'error': np.finfo(float).max, # set Error to max float val
+                'i': 0}        
+        
+        while True:
+            candidate_transition_time = self.data['timestamp'][self.steady_state_transitions[i][0]]
+            
+            target_transition_time = target_transition[0]+start_timestamp
+            
+            if candidate_transition_time > target_transition_time+20:
+                break
+            
+            error = abs(candidate_transition_time - target_transition_time) + abs(self.steady_state_transitions[i][2] - target_transition[2])   
+            
+            if error < best['error']:
+                best['error'] = error
+                best['i'] = i
+            
+            i += 1
+            
+        return best     
     
     @property
     def duration(self):
@@ -120,16 +199,13 @@ class WattsUp(object):
         self.data = np.loadtxt(open(filename, 'rU'), delimiter="\t", dtype=float, skiprows=3, usecols=[1])
         self._trim()
         self.steady_states = calc_steady_states(self.data)
-        self.steady_state_transitions = self._calc_good_steady_state_transitions()
+        self.steady_state_transitions = calc_good_steady_state_transitions(self.steady_states)
 
     def plot(self, axes, start_timestamp=0):
         x = [datetime.datetime.fromtimestamp(t+start_timestamp) for t in range(0, self.data.size, self.period)]
-        axes.plot(x, self.data)
+        axes.plot(x, self.data/1000, color='g', label="WattsUp")
         
-    def plot_steady_states(self, axes):
-        for start, end, value in self.steady_states:
-            line = matplotlib.lines.Line2D([start, end], [value, value], color='r')
-            axes.add_line(line)
+
 
     @property
     def duration(self):
@@ -171,30 +247,51 @@ class WattsUp(object):
 
         return states
     
-    def _calc_good_steady_state_transitions(self):
-        """Good state transitions are those which are large in magnitude
-        and where the two states are very close"""
-        MIN_SCORE = 10
+    
+def plot_three():
+    iam = MeterSignal("/home/jack/Dropbox/Data/data2/channel_5.dat")
+    whole_house = MeterSignal("/home/jack/Dropbox/Data/data2/channel_1.dat")
+    target = WattsUp("/home/jack/Dropbox/Data/BellendenWMNov2012.TXT")    
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(3,1,1)
+    ax2 = fig.add_subplot(3,1,2)
+    ax3 = fig.add_subplot(3,1,3)
+    
+    
+    #ax.plot(target.data)
+    #plot_steady_states(ax, target.steady_states)
+    
+    #ax.plot(haystack.data['watts'])
+    #plot_steady_states(ax, haystack.steady_states)    
+
+    iam.plot(ax1)
+    whole_house.plot(ax2)
+    t=1352822455  # correct start time for WM target is 1352822455 in data2 
+    target.plot(ax3,t)
+     
+    ax3.set_xlabel("time")
+    ax2.set_ylabel("power (kw)")
+    
+    date_formatter = matplotlib.dates.DateFormatter("%H:%M")
+    date_formatter_none = matplotlib.dates.DateFormatter("")
+    ax1.xaxis.set_major_formatter( date_formatter_none )
+    ax2.xaxis.set_major_formatter( date_formatter_none )
+    ax3.xaxis.set_major_formatter( date_formatter )
+    
+    ax1.set_xlim( ax3.get_xlim() )
+    ax2.set_xlim( ax3.get_xlim() )
+    ax1.set_ylim( ax3.get_ylim() )
+    ax2.set_ylim([0, 3])
+    
+    ax1.set_title("EDF IAM")
+    ax2.set_title("EDF whole house")
+    ax3.set_title("WattsUp")
+    
+    plt.show()
         
-        ss_transitions = [] # list of tuples for transition: start, length, value, score
-        last_ss_end = 0
-        last_ss_value = 0
-        for ss_start, ss_end, ss_value in self.steady_states:
-            t_length = (ss_start - last_ss_end) * self.period
-            if t_length == 0:
-                t_length = 1
-            t_value = ss_value - last_ss_value
-            t_score = abs(t_value) if t_length < 18 else abs(t_value)/t_length
-            last_ss_end = ss_end * self.period
-            last_ss_value = ss_value
-            
-            if t_score > MIN_SCORE:
-                ss_transitions.append((last_ss_end, t_length, t_value, t_score))
-                            
-        return sorted(ss_transitions, key=lambda score: score[3], reverse=True)
 
-
-if __name__ == "__main__":
+def auto():
     haystack = MeterSignal("/home/jack/Dropbox/Data/data2/channel_5.dat")
     target = WattsUp("/home/jack/Dropbox/Data/BellendenWMNov2012.TXT")
     
@@ -205,12 +302,21 @@ if __name__ == "__main__":
     print(best)
     
     fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    
-    #ax.plot(target.data)
-    #target.plot_steady_states(ax)
-    
-    haystack.plot(ax)
-    target.plot(ax, haystack.data['timestamp'][best['start_index']])
+    ax1 = fig.add_subplot(1,1,1)
+
+    haystack.plot(ax1)
+    t= haystack.data['timestamp'][haystack.steady_state_transitions[best['start_index']][0]]
+    print("t",t)
+    target.plot(ax1,t) 
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("power (kw)")
+    date_formatter = matplotlib.dates.DateFormatter("%d/%m\n%H:%M")
+    ax1.xaxis.set_major_formatter( date_formatter )
+    ax1.legend()
     plt.show()
+    
+
+if __name__ == "__main__":
+    plot_three()
+    # auto()
     
